fa97fda9464b6c397fb446a5df2ba31625d3592f
adiser <sergiadipraja@gmail.com>
2023-12-01 15:03:45 -0500

    Loss (error) and convergence criteria.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple, Dict
+from typing import Dict
 
 import cv2
 import numpy as np
@@ -7,14 +7,20 @@ from pytorch3d.loss import chamfer_distance
 from scipy.spatial.transform import Rotation
 from torch import nn
 
-from calibration import Kitti, get_initialized_input
-from utils import LabelConfig, DOWNSCALING_FACTOR
+from calibration import Kitti
+from utils import LabelConfig, DOWNSCALING_FACTOR, convergence_criteria_frame, convergence_criteria, EarlyStopper, \
+    choose_samples, pred_and_visualize, LidarSample
 from models.utils import get_model
 
 
-class LidarSample(NamedTuple):
-    lidar_points: np.ndarray
-    lidar_labels: np.ndarray
+def get_error(pred_extrinsics, gt_extrinsics):
+    pred_rotation_matrix = pred_extrinsics[:, :3]
+
+    delta_rotation_matrix = np.linalg.inv(pred_rotation_matrix) @ gt_extrinsics[:, :3]
+    delta_rotation = Rotation.from_matrix(delta_rotation_matrix)
+    delta_rotation_euler = delta_rotation.as_euler('ZYX', degrees=True)
+
+    return delta_rotation_euler
 
 
 def main_bundle_adjust():
@@ -34,18 +40,33 @@ def main_bundle_adjust():
     translation_meters = dict(z=0.0, y=-0.0, x=-0.0)
     num_iter = 1000
     image_labels_subsampling_factor = 5.
-    depth_scaling_factor = 0.1
-    num_samples = 16
+    depth_scaling_factor = 1
+    sample_range = (0, 1)
+    num_samples = 1
     data_dump_dir = "../data/batch_test/"
     translation_upweighting = 0
+
+    # Visualization
+    sample_to_visualize = 0
+
+    # Convergence stuff
+    convergence_criteria_frame_id = 525
+    patience = 4
+    convergence_threshold = 0.01
+    min_delta = 0.001
+
+    early_stopper = EarlyStopper(patience=patience, min_delta=min_delta, convergence_threshold=convergence_threshold)
 
     # Just get the initialized extrinsics from the projection of the first kitti sample
     _, _, _, init_extrinsics, _ = kitti.get_rotated_projection(
         idx=0, camera_id=camera_id, rotation_degrees=rotation_degrees,
         translation_meters=translation_meters
     )
-
     _, _, _, gt_extrinsics, _ = kitti.get_gt_projection(idx=0, camera_id=2)
+
+    # Choose a particular frame as convergence criteria
+    key_lidar_data, key_lidar_labels, annotated_gt_points_dict = convergence_criteria_frame(
+        kitti, convergence_criteria_frame_id, camera_id)
 
     # Modeling components.
     calibrator_model = get_model(init_extrinsics=init_extrinsics, rot_param_type=rot_param_type)
@@ -57,26 +78,16 @@ def main_bundle_adjust():
     for iteration in range(num_iter):
 
         total_chamdist = 0
-        samples = list(np.random.choice(list(range(0, 200)), num_samples))
 
-        lidar_samples = [
-            LidarSample(lidar_points=kitti.load_lidar_points(i),
-                        lidar_labels=kitti.load_lidar_labels(i)) for i in samples
-        ]
-
-        gt_image_point_clouds = [
-            kitti.get_image_semlabels_with_depth(
-                idx=i, subsampling_factor=image_labels_subsampling_factor, camera_id=camera_id,
-                depth_scaling_factor=depth_scaling_factor)[1] for i in samples
-        ]
-
-        gt_images = [
-            kitti.load_image(idx=i, camera_id=camera_id) for i in samples
-        ]
+        # Choose samples.
+        lidar_samples, gt_image_point_clouds, gt_images = choose_samples(sample_range,
+                                                                         num_samples,
+                                                                         kitti,
+                                                                         image_labels_subsampling_factor,
+                                                                         camera_id,
+                                                                         depth_scaling_factor)
 
         # Choose image to plot.
-        which_image_to_display = 0
-        image = gt_images[which_image_to_display][:, :, ::-1].copy()
 
         for i, (lidar_sample, gt_image_point_cloud) in enumerate(zip(lidar_samples, gt_image_point_clouds)):
 
@@ -103,24 +114,11 @@ def main_bundle_adjust():
             assert sorted(LabelConfig.labels()) == sorted(gt_image_point_cloud.keys())
 
             for class_name in LabelConfig.labels():
-
                 pred_points = pred_point_cloud[class_name]
                 gt_points = gt_image_point_cloud[class_name]
 
-                # Draw the points on the first image.
-                # if i == 0:
-                #     pred_color = LabelConfig.color(class_name)[::-1]
-                #     gt_color = LabelConfig.color(class_name)
-                #     image[pred_points[:, 1].int(), pred_points[:, 0].int(), :] = np.array(pred_color)
-                #     image[gt_points[:, 1].int(), gt_points[:, 0].int(), :] = np.array(gt_color)
-
                 chamdist, _ = chamfer_distance(gt_points[None, :], pred_points[None, :], single_directional=False)
-                # chamdist = chamdist / len(gt_points) * 4000
                 total_chamdist = total_chamdist + chamdist
-
-
-        # Dump the image
-        # cv2.imwrite(f'{data_dump_dir}/iteration_{iteration}.png', image[:, :, ::-1])
 
         # Downscale the value for numerical stability and average them per sample
         total_chamdist = total_chamdist / DOWNSCALING_FACTOR / num_samples
@@ -130,7 +128,7 @@ def main_bundle_adjust():
         optimizer.zero_grad()
         loss.backward()
 
-        # Upweight the translation update
+        # To perform translation update.
         for name, param in calibrator_model.named_parameters():
             if name == 'translation_param':
                 if param.grad is not None:
@@ -138,10 +136,21 @@ def main_bundle_adjust():
 
         optimizer.step()
 
+        pred_and_visualize(sample_to_visualize,
+                           kitti,
+                           calibrator_model,
+                           image_labels_subsampling_factor,
+                           camera_id,
+                           depth_scaling_factor,
+                           projection_matrix,
+                           image_height,
+                           image_width,
+                           data_dump_dir,
+                           iteration)
+
         # Visualize it on the first sample:
         lidar_points = torch.from_numpy(kitti.load_lidar_points(0))
         lidar_labels = torch.from_numpy(kitti.load_lidar_labels(0).astype(int))
-
         gt_image_point_clouds_test = kitti.get_image_semlabels_with_depth(
             idx=0, subsampling_factor=image_labels_subsampling_factor, camera_id=camera_id,
             depth_scaling_factor=depth_scaling_factor)[1]
@@ -168,20 +177,36 @@ def main_bundle_adjust():
 
         cv2.imwrite(f'{data_dump_dir}/iteration_{iteration}.png', gt_image_vis[:, :, ::-1])
 
-        print(f"Iteration {iteration}: {loss.item()}, {calibrator_model.rot_param.data} {calibrator_model.translation_param.data}")
-        print(f'Error translation: {torch.from_numpy(gt_extrinsics[:, 3]) - calibrator_model.translation_param.data }')
+        print(
+            f"Iteration {iteration}: {loss.item()}, {calibrator_model.rot_param.data} {calibrator_model.translation_param.data}")
+        print(f'Error translation: {torch.from_numpy(gt_extrinsics[:, 3]) - calibrator_model.translation_param.data}')
 
-        pred_rotation_matrix = calibrator_model.construct_extrinsics_matrix()[:, :3]
-        pred_rotation = Rotation.from_matrix(pred_rotation_matrix)
-        pred_rotation_euler = pred_rotation.as_euler('ZYX', degrees=True)
+        # Get the error
+        pred_extrinsics = calibrator_model.construct_extrinsics_matrix()
+        delta_rotation_euler = get_error(pred_extrinsics=pred_extrinsics, gt_extrinsics=gt_extrinsics)
 
-        gt_rotation = Rotation.from_matrix(gt_extrinsics[:, :3])
-        gt_rotation_euler = gt_rotation.as_euler('ZYX', degrees=True)
+        if loss.item() < 20:
+            calibrator_model = get_model(init_extrinsics=pred_extrinsics, rot_param_type='quaternion')
+            optimizer = torch.optim.SGD(calibrator_model.parameters(), lr=1e-6)
 
-        error_rotation = np.abs(pred_rotation_euler - gt_rotation_euler)
+        # Infer in convergence criteria frame.
+        error_criteria, converged = convergence_criteria(calibrator_model=calibrator_model,
+                                                         key_lidar_data=key_lidar_data,
+                                                         key_lidar_labels=key_lidar_labels,
+                                                         annotated_gt_points_dict=annotated_gt_points_dict,
+                                                         projection_matrix=projection_matrix,
+                                                         image_height=image_height,
+                                                         image_width=image_width,
+                                                         early_stopper=early_stopper)
 
-        print(f"\t Error rotation: z: {error_rotation[0]}, y: {error_rotation[1]}, z: {error_rotation[2]}")
+        print(
+            f"Error rotation: z: {delta_rotation_euler[0]}, y: {delta_rotation_euler[1]}, x: {delta_rotation_euler[2]}")
+        print(f"Convergence criteria: {error_criteria}")
 
+        if converged:
+            assert converged in [1, 2]
+            convergence_criteria_dict = {1: "Patience", 2: "Convergence threshold"}
+            raise SystemExit(f"Converged with criteria {converged}: {convergence_criteria_dict[converged]}")
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,12 @@
+from typing import Dict, NamedTuple
+
+import cv2
+import numpy as np
+import torch
 import yaml
+from fvcore import nn
+from pytorch3d.loss import chamfer_distance
+from torch.nn import MSELoss
 
 
 class LabelConfig:
@@ -52,3 +60,142 @@ def load_semkitti_config(data_source: str = '../data/semantic-kitti.yaml'):
     return yaml_dict
 
 
+def convergence_criteria_frame(kitti, convergence_criteria_frame, camera_id):
+    label_img, pts_2d_fov, pts_label_fov, tr, lidar_data = kitti.get_gt_projection(idx=convergence_criteria_frame,
+                                                                                   camera_id=camera_id)
+    annotated_gt_points_dict = dict()
+    for label in LabelConfig.labels():
+        pts_2d = torch.from_numpy(pts_2d_fov[pts_label_fov == label])
+        pts_2d = torch.hstack([pts_2d, torch.zeros(len(pts_2d))[:, None]]).float()
+        annotated_gt_points_dict[label] = pts_2d
+
+    lidar_labels = kitti.load_lidar_labels(convergence_criteria_frame)
+    return lidar_data, lidar_labels, annotated_gt_points_dict
+
+class EarlyStopper:
+    def __init__(self, patience=3, min_delta=0.001, convergence_threshold: float = 0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.min_validation_loss = float('inf')
+        self.convergence_threshold = convergence_threshold
+
+    def early_stop(self, validation_loss):
+        if validation_loss < self.min_validation_loss:
+            self.min_validation_loss = validation_loss
+            self.counter = 0
+        elif validation_loss > (self.min_validation_loss + self.min_delta):
+            self.counter += 1
+            if self.counter >= self.patience:
+                return 1
+
+        if validation_loss < self.convergence_threshold:
+            return 2
+
+        return 0
+
+def convergence_criteria(calibrator_model,
+                         key_lidar_data,
+                         key_lidar_labels,
+                         annotated_gt_points_dict,
+                         projection_matrix,
+                         image_height,
+                         image_width,
+                         early_stopper: EarlyStopper):
+
+    lidar_data = torch.from_numpy(key_lidar_data)
+    lidar_labels = torch.from_numpy(key_lidar_labels.astype(int))
+
+    pred_point_cloud: Dict[int, torch.Tensor] = calibrator_model(lidar_data=lidar_data,
+                                                                 projection_matrix=projection_matrix,
+                                                                 image_height=image_height,
+                                                                 image_width=image_width,
+                                                                 label_data=lidar_labels,
+                                                                 add_depth=True,
+                                                                 depth_scaling_factor=0)
+
+    total_chamdist = 0
+    for class_name in LabelConfig.labels():
+        pred_points = pred_point_cloud[class_name]
+        gt_points = annotated_gt_points_dict[class_name]
+
+        chamdist, _ = chamfer_distance(gt_points[None, :], pred_points[None, :], single_directional=False)
+        total_chamdist = total_chamdist + chamdist
+
+    total_chamdist = total_chamdist / DOWNSCALING_FACTOR
+
+    converged = early_stopper.early_stop(total_chamdist)
+
+    return total_chamdist, converged
+
+def choose_samples(sample_range,
+                   num_samples,
+                   kitti,
+                   image_labels_subsampling_factor,
+                   camera_id,
+                   depth_scaling_factor):
+
+    samples = list(np.random.choice(list(range(sample_range[0], sample_range[1])), num_samples))
+
+    lidar_samples = [
+        LidarSample(lidar_points=kitti.load_lidar_points(i),
+                    lidar_labels=kitti.load_lidar_labels(i)) for i in samples
+    ]
+
+    gt_image_point_clouds = [
+        kitti.get_image_semlabels_with_depth(
+            idx=i, subsampling_factor=image_labels_subsampling_factor, camera_id=camera_id,
+            depth_scaling_factor=depth_scaling_factor)[1] for i in samples
+    ]
+
+    gt_images = [
+        kitti.load_image(idx=i, camera_id=camera_id) for i in samples
+    ]
+
+    return lidar_samples, gt_image_point_clouds, gt_images
+
+def pred_and_visualize(idx,
+                       kitti,
+                       calibrator_model,
+                       image_labels_subsampling_factor,
+                       camera_id,
+                       depth_scaling_factor,
+                       projection_matrix,
+                       image_height,
+                       image_width,
+                       data_dump_dir,
+                       iteration):
+
+    # Visualize it on the first sample:
+    lidar_points = torch.from_numpy(kitti.load_lidar_points(idx))
+    lidar_labels = torch.from_numpy(kitti.load_lidar_labels(idx).astype(int))
+    gt_image_point_clouds_test = kitti.get_image_semlabels_with_depth(
+        idx=0, subsampling_factor=image_labels_subsampling_factor, camera_id=camera_id,
+        depth_scaling_factor=depth_scaling_factor)[1]
+    gt_image = kitti.load_image(idx=idx, camera_id=camera_id)
+    gt_image_vis = gt_image.copy()[:, :, ::-1]
+
+    pred_point_cloud: Dict[int, torch.Tensor] = calibrator_model(lidar_data=lidar_points,
+                                                                 projection_matrix=projection_matrix,
+                                                                 image_height=image_height,
+                                                                 image_width=image_width,
+                                                                 label_data=lidar_labels,
+                                                                 add_depth=True,
+                                                                 depth_scaling_factor=depth_scaling_factor)
+
+    for class_name in LabelConfig.labels():
+        pred_points = pred_point_cloud[class_name]
+        gt_points = gt_image_point_clouds_test[class_name]
+
+        # Draw the points on the first image.
+        pred_color = LabelConfig.color(class_name)[::-1]
+        gt_color = LabelConfig.color(class_name)
+        gt_image_vis[pred_points[:, 1].int(), pred_points[:, 0].int(), :] = np.array(pred_color)
+        gt_image_vis[gt_points[:, 1].astype(int), gt_points[:, 0].astype(int), :] = np.array(gt_color)
+
+    cv2.imwrite(f'{data_dump_dir}/iteration_{iteration}.png', gt_image_vis[:, :, ::-1])
+
+
+class LidarSample(NamedTuple):
+    lidar_points: np.ndarray
+    lidar_labels: np.ndarray
