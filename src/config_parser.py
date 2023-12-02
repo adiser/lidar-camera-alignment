2bbeab874772c04bbafc77cea65bb2729bbd7b51
hari2181998 <harishankar2181998@gmail.com>
2023-12-02 16:43:02 -0500

    Added config file for running experiments

---
+++ src/config_parser.py
@@ -0,0 +1,25 @@
+import json
+
+class ConfigParser:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.parse_config()
+
+    def parse_config(self):
+        with open(self.filepath, 'r') as file:
+            config = json.load(file)
+            self.dict = config
+        
+        for key, value in config.items():
+            setattr(self, key, value)
+    
+    def get_dict(self):
+        return self.dict
+
+
+# Usage example
+config_parser = ConfigParser('/home/hari/Documents/CMU_masters/targetless_lidar_camera_calibration/lidar-camera-alignment/src/cfg/config.json')
+print(config_parser.rot_param_type)
+print(config_parser.use_depth)
+print(config_parser.lr)
+# Access other variables in a similar way
